ce2c5cf0457f5386e40bb882d06966d0dbc45245
Topiinho <vitoralcantara1722@gmail.com>
2025-02-10 18:18:22 -0300

    Update 10/02/2025

--- app/views/interface.py
+++ app/views/interface.py
@@ -1,37 +1,21 @@
 import flet as ft
+from menuSideBar import create_sidebar
 
 def main(page: ft.Page):
     page.bgcolor = ft.colors.GREY_800
 
-    leftSideBarNavi = ft.Container(
-        padding=10,
-        height=1080,
-        border_radius=5,
-        col= 1,
-        bgcolor= ft.colors.BLUE_GREY_900,
-        content=ft.Column(
+    sidebar, toggle_sidebar = create_sidebar(page)
+
+    layout = ft.Container(
+        content=ft.Row(
             controls=[
-                ft.Container(
-                    padding=1,
-                    content=ft.Image(src="Sync_Logo.png"),
-                    bgcolor=ft.colors.WHITE,
-                    shape=ft.BoxShape.CIRCLE,
-                    height= 50,
-                    alignment=ft.alignment.center
-                )
+                sidebar,
+                ft.Container(expand=True, bgcolor=ft.colors.GREY_700),
             ]
         )
     )
 
-    layout = ft.Container(
-        width=1920,
-        height=1080,
-        content=ft.Row(
-            controls=[
-                leftSideBarNavi,
-            ]
-        )
-    )
+    sidebar.on_hover = toggle_sidebar
 
     page.add(layout)
 
